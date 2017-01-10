80b06e9735553d237e86a7cb1c57ad7140c539b4
James Balajan <jbalajan@gmail.com>
2017-01-10 13:48:42 +1100

    Delete Trail.cpp

--- Trail.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by JULIA BALAJAN on 30/12/2016.
-//
-
-#include "Trail.h"
-
-Trail::Trail(Team &nteam, int8_t x, int8_t y) : team(nteam), pos(x, y) {}
-
-Trail::Trail(Team &nteam, Coord npos) : team(nteam), pos(npos) {}
